9b03287bca00997f4b6cf9648df65c0a7f905f8a
Anticife <ajhurtap@gmail.com>
2026-05-12 00:08:53 -0500

    chore: build production assets with updated configuration

--- build/1957.27537633.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkproyecto_web1_backend=self.webpackChunkproyecto_web1_backend||[]).push([[1957],{91957(b,e,c){c.r(e),c.d(e,{default:()=>u});const u={}}}]);
---
+++ build/4338.c57cd195.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkproyecto_web1_backend=self.webpackChunkproyecto_web1_backend||[]).push([[4338],{91957(b,e,c){c.r(e),c.d(e,{default:()=>u});const u={}}}]);
