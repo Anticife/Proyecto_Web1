a9e03e02ba64f6ce68cf9fc5c514e0f6a33ac17c
Anticife <ajhurtap@gmail.com>
2026-05-12 01:48:36 -0500

    fix: add missing service files for property and payments APIs

---
+++ src/api/payments/services/payment.js
@@ -0,0 +1,5 @@
+'use strict';
+
+// payments doesn't use a core factory since the controller handles everything directly,
+// but Strapi requires a service file for the API to be registered correctly.
+module.exports = {};
